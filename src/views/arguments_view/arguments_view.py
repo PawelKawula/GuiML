b959169813450bef6f53fb35713a6ed58f503060
Pawel Kawula <p4wos8000@gmail.com>
2022-10-12 21:13:06 +0200

    arg view recursive keys for items and refactor

--- src/views/arguments_view/arguments_view.py
+++ src/views/arguments_view/arguments_view.py
@@ -2,17 +2,16 @@ from gi.repository import Gtk
 
 from learning.defined_models import learn_models
 from .argument_item import ArgumentItem
+from .util import get_recursive_dict_item, get_recursive_dict_item_from_toml
 from learning.ml_model import MlModel
 
 
 class ArgumentsView(Gtk.VBox):
     def __init__(self, view, ml_model_name, parse_options=["learning"]):
         super().__init__()
-        self.items, self.expanders, self.vboxes = {}, [], []
-        self.method_args = {}
+        self.items = {}
         self.view = view
         for option in parse_options:
-            print(option)
             arguments = learn_models[ml_model_name].parse_options(option)
             for method, arguments in arguments.items():
                 view, expand = self, True
@@ -20,23 +19,24 @@ class ArgumentsView(Gtk.VBox):
                     view = Gtk.Frame()
                     self.add(view)
                     expand = False
-                self.add_sublists(method, arguments, view, 0, expand)
+                self.items[option] = {}
+                self.add_sublists([option, method], arguments, view, 0, expand)
 
     def add_sublists(self, method, arguments, view, margin, expand=False):
-        expander, vbox = Gtk.Expander(label=method, margin_left=margin), Gtk.VBox()
-        self.method_args[method] = {}
+        expander, vbox = (
+            Gtk.Expander(label=method[-1], margin_left=margin, valign=Gtk.Align.START),
+            Gtk.VBox(),
+        )
+        get_recursive_dict_item(self.items, method, 1)[method[-1]] = {}
         for name, widget_info in arguments.items():
             if "widget_type" in widget_info:
-                self.add_item(vbox, method, name, widget_info)
+                self.add_item(vbox, method + [name], name, widget_info)
             else:
-                self.add_sublists(name, widget_info, vbox, margin + 10)
-        # self.view.get_content_area().add(expander)
+                self.add_sublists(method + [name], widget_info, vbox, margin + 10)
         expander.add(vbox)
         view.add(expander)
         if expand:
             expander.set_expanded(True)
-        # self.expanders.append(expander)
-        # self.vboxes.append(vbox)
 
     def add_item(self, vbox, method, name, widget_info):
         widget_type = widget_info["widget_type"]
@@ -44,41 +44,33 @@ class ArgumentsView(Gtk.VBox):
         values = widget_info.get("values", None)
         widget_type = MlModel.parse_widget_type(widget_type)
         item = ArgumentItem(name, widget_type, data_type, values)
-        self.items[f"{method}.{item.name}"] = item
+        dict_item = get_recursive_dict_item(self.items, method, 1)
+        dict_item[method[-1]] = item
+        self.set_item_attribs(item, widget_info)
+        vbox.pack_start(item, True, True, 0)
+
+    def set_item_attribs(self, item, widget_info):
         if "default" in widget_info:
             item.set_default(widget_info["default"])
         if "enabled_on" in widget_info:
             enabled_on = widget_info["enabled_on"]
-            dis_item = self.items[f"{method}.{name}"]
-            self.items[enabled_on["argument"]].add_enabled_on(
-                (dis_item, enabled_on["value"])
-            )
-            dis_item.set_widget_sensitive(False)
+            get_recursive_dict_item_from_toml(
+                self.items, enabled_on["argument"]
+            ).add_enabled_on((item, enabled_on["value"]))
+            item.set_widget_sensitive(False)
         if "visible_on" in widget_info:
-            visible_on = widget_info["enabled_on"]
-            dis_item = self.items[f"{method}.{name}"]
-            self.items[visible_on["argument"]].add_visible_on(
-                (dis_item, visible_on["value"])
-            )
-            dis_item.set_widget_visible(False)
-        vbox.pack_start(item, True, True, 0)
-        self.method_args[method][name] = item
+            visible_on = widget_info["visible_on"]
+            get_recursive_dict_item_from_toml(
+                self.items, visible_on["argument"]
+            ).add_visible_on((item, visible_on["value"]))
+            item.set_widget_visible(False)
 
-    def get_arguments(self):
-        return {
-            method: {
-                name: item.get_value()
-                for name, item in args.items()
-                if item.get_sensitive()
-            }
-            for method, args in self.method_args.items()
-        }
-
-    """
-    def destroy(self):
-        for items in [self.vboxes, self.expanders]:
-            for item in items:
-                item.destroy()
-        for item in self.items.values():
-            item.destroy()
-    """
+    def get_arguments(self, items=None):
+        items = self.items if items is None else items
+        args = {}
+        for level, item in items.items():
+            if isinstance(item, dict):
+                args.update({level: self.get_arguments(item)})
+            else:
+                args.update({level: item.get_value()})
+        return args
---
+++ src/views/arguments_view/util.py
@@ -0,0 +1,12 @@
+def get_recursive_dict_item(d, keys, to_end=None):
+    to_end = -len(keys) if to_end is None else to_end
+    for key in keys[:-to_end]:
+        d = d[key]
+    return d
+
+
+def get_recursive_dict_item_from_toml(d, keys, to_end=None):
+    keys = keys.split(".")
+    print(d)
+    print(keys)
+    return get_recursive_dict_item(d, keys, to_end)
