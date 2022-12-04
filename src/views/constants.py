af91748795cb741f376b57dd24ab93bb470bff65
Pawel Kawula <p4wos8000@gmail.com>
2022-12-04 19:16:26 +0100

    fixed moved glade files

--- src/views/constants.py
+++ src/views/constants.py
@@ -5,17 +5,18 @@ from gi.repository import Gtk
 
 SRC_PATH = Path(os.path.realpath(__file__)).parent.parent
 PROJECT_ROOT = SRC_PATH.parent
+GLADE_ROOT = PROJECT_ROOT / "glade"
 
-GLADE_FILE = str(PROJECT_ROOT / "project.glade")
-ABOUT_FILE = str(PROJECT_ROOT / "about.glade")
-FILE_CHOOSER_FILE = str(PROJECT_ROOT / "file_chooser.glade")
-RESULT_FILE = str(PROJECT_ROOT / "result.glade")
-RESULT_ADD_FILE = str(PROJECT_ROOT / "result_add.glade")
-LEARN_DIALOG_FILE = str(PROJECT_ROOT / "learn_dialog.glade")
-SETTINGS_FILE = str(PROJECT_ROOT / "settings.glade")
-SETTINGS_TAB_FILE = str(PROJECT_ROOT / "settings_tab.glade")
-SPLITS_FILE = str(PROJECT_ROOT / "splits.glade")
-DATETIME_FILE = str(PROJECT_ROOT / "datetime.glade")
+GLADE_FILE = str(GLADE_ROOT / "project.glade")
+ABOUT_FILE = str(GLADE_ROOT / "about.glade")
+FILE_CHOOSER_FILE = str(GLADE_ROOT / "file_chooser.glade")
+RESULT_FILE = str(GLADE_ROOT / "result.glade")
+RESULT_ADD_FILE = str(GLADE_ROOT / "result_add.glade")
+LEARN_DIALOG_FILE = str(GLADE_ROOT / "learn_dialog.glade")
+SETTINGS_FILE = str(GLADE_ROOT / "settings.glade")
+SETTINGS_TAB_FILE = str(GLADE_ROOT / "settings_tab.glade")
+SPLITS_FILE = str(GLADE_ROOT / "splits.glade")
+DATETIME_FILE = str(GLADE_ROOT / "datetime.glade")
 
 MARGINS = {
     "margin_top": 20,
